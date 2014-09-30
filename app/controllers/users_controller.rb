eb85e382ac748e91aeac8c17b1c528c930cb0d28
Nick McLaughlin <mclaughlin.nick@gmail.com>
2014-09-30 17:56:22 -0400

    1) add nav bar 2) allow current user to see complete profile in nav bar if they havent started to fill it out 3) once user completes profile they then see edit profile tab along with your profile tab to view how it looks 4) put picture on welcome page that will change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def update
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-      flash[:success] = "You have successfully updated your review."
+      flash[:success] = "You have successfully updated your profile."
       redirect_to root_path
     else
       render 'edit'
     end
   end
+
 
   private
   def user_params
