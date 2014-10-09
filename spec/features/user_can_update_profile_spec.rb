589de28c204ea9bb143f3000745e4297da47ac03
Nick McLaughlin <mclaughlin.nick@gmail.com>
2014-10-09 13:19:40 -0400

    1) add test for uploading photo 2) add test for designating photo as profile picture

--- spec/features/user_can_update_profile_spec.rb
+++ spec/features/user_can_update_profile_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature "After log in user can update profile" do
+feature "After log in user can complete profile then update it" do
   scenario "User can update profile" do
     user = FactoryGirl.create(:user)
 
@@ -19,7 +19,51 @@ feature "After log in user can update profile" do
 
     expect(page).to have_content("You have successfully updated your profile.")
 
+    click_on "Edit Profile"
+
+    fill_in("Profession", with: "Coder")
+    fill_in("Bio", with: "Man")
+    fill_in("Zip", with: "02322")
+    fill_in("Interests", with: "Wow")
+
+    click_on "Update Profile"
+
+    expect(page).to have_content("You have successfully updated your profile.")
   end
+
+  scenario "User can submit photo" do
+    user = FactoryGirl.create(:user)
+    sign_in_as(user)
+    visit edit_user_path(user)
+
+    attach_file "Photo", Rails.root.join('spec/fixtures/download.jpeg')
+
+    click_on "Submit Photo"
+
+    expect(page).to have_content("You have successfully added a photo.")
+  end
+
+  scenario "User can submit photo then designate it as a profile picture" do
+    user = FactoryGirl.create(:user)
+    sign_in_as(user)
+    visit edit_user_path(user)
+
+
+    attach_file "Photo", Rails.root.join('spec/fixtures/download.jpeg')
+
+    click_on "Submit Photo"
+
+    expect(page).to have_content("You have successfully added a photo.")
+
+    click_on "More Photos"
+
+    click_on "Make Profile Picture"
+
+    select('true', :from => 'Profile photo')
+    click_on "Make Profile Picture"
+    expect(page).to have_content("You have successfully updated your photo.")
+  end
+
 
   # scenario "User updates zip blank" do
   #   user = FactoryGirl.create(:user)
