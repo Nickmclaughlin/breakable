1a0b3744e9be3da711c10388758f2b975eaa9d0b
Nick McLaughlin <mclaughlin.nick@gmail.com>
2014-12-30 14:22:55 -0500

    codeship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+ vclass User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
 
 
   STATES = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
-  AGE = [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28 , 29, 40, 31, 32, 33, 34, 35, 36, 37, 38 , 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 60, 51, 52, 53, 54, 55, 56, 57, 58 ,59, 60, 61, 62, 63, 64, 65, 66, 67, 68 ,69]
+  AGE = [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28 , 29, 40, 31, 32, 33, 34, 35, 36, 37, 38 , 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 60, 51, 52, 53, 54, 55, 56, 57, 58 ,59, 60, 61, 62, 63, 64, 65, 66, 67, 68 ,69]
   HEIGHT = ["4' 10\"", "4' 11\"", "5' 0\"", "5' 1\"", "5' 2\"", "5' 3\"", "5' 4\"", "5' 4\"", "5' 6\"", "5' 7\"", "5' 8\"", "5' 9\"", "5' 11\"", "6' 0\"", "6' 1\"", "6' 2\"", "6' 3\"", "6' 4\"", "6' 5\"", "6' 6\"", "6' 7\"", "6' 8\"", "6' 9\"", "6' 10\""]
 
   def self.search(search)
